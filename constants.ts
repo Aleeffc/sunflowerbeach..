b83567c80fdaf7c7432d6096758871d7f25cd65d
Alef Cerqueira <126865788+Aleeffc@users.noreply.github.com>
2025-11-22 00:03:19 -0300

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,6 +1,12 @@
 
 import { Product, Category, User, SiteSettings } from './types';
 
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  'https://lqczpzglmvxaqekkctih.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxY3pwemdsbXZ4YXFla2tjdGloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3NzI4ODUsImV4cCI6MjA3OTM0ODg4NX0.rjEgTFlNz4M_TKpktVpi8cdesJ3Eaparub3VWMDJ5YM'
+)
 export const INITIAL_USERS: User[] = [
   {
     id: 'admin-1',
